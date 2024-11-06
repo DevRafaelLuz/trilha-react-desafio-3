011a9abf447bcbe9ab26d858f48b77acefb1484c
DevRafaelLuz <rafael.luz2005@gmail.com>
2024-11-06 14:25:39 -0300

    Registration page developed

--- src/pages/cadastro/index.jsx
+++ src/pages/cadastro/index.jsx
@@ -8,7 +8,7 @@ import { api } from '../../services/api';
 import { useForm } from "react-hook-form";
 
 
-import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
+import { Container, Title, Column, TitleLogin, SubtitleLogin, JaTenhoContaHighlight, Wrapper } from './styles';
 
 const Login = () => {
 
@@ -40,24 +40,23 @@ const Login = () => {
         <Header />
         <Container>
             <Column>
-                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
-                 e entrar mais rápido nas empresas mais desejadas.</Title>
+                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias e entrar mais 
+                    rápido nas empresas mais desejadas.</Title>
             </Column>
             <Column>
                 <Wrapper>
-                <TitleLogin>Faça seu cadastro</TitleLogin>
-                <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                <TitleLogin>Começe agora grátis</TitleLogin>
+                <SubtitleLogin>Crie sua conta e make the change._</SubtitleLogin>
                 <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input type="text" placeholder="Nome completo" />
                     <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
                     {errors.email && <span>E-mail é obrigatório</span>}
                     <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
                     {errors.senha && <span>Senha é obrigatório</span>}
-                    <Button title="Entrar" variant="secondary" type="submit"/>
+                    <Button title="Criar minha conta" variant="secondary" type="submit"/>
                 </form>
-                <Row>
-                    <EsqueciText>Esqueci minha senha</EsqueciText>
-                    <CriarText>Criar Conta</CriarText>
-                </Row>
+                Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas de Privacidade e os Termos de uso da DIO.<br/>
+                já tenho conta<JaTenhoContaHighlight>Fazer login</JaTenhoContaHighlight>
                 </Wrapper>
             </Column>
         </Container>
